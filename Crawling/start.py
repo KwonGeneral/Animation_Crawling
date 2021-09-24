f891c3d5b27866c4e75c763d22b7cc6111b62fb0
KwonGeneral <thtm9020@gmail.com>
2021-09-24 21:41:55 +0900

    0924 update2

--- Crawling/start.py
+++ Crawling/start.py
@@ -5,36 +5,22 @@ from openpyxl import load_workbook
 
 from Crawling.ani_detail_crawl import namuwiki_crawl
 
-work_book = load_workbook("ani_b.xlsx")
-sheet = work_book['Sheet']
 
-# 나무위키 크롤링 1)
-for no in range(2, len(sheet["A"]) + 1):
-    namuwiki_crawl(work_book, "ani_b.xlsx", sheet["H"+str(no)].value, sheet["B"+str(no)])
+print("\n 1 : 나무위키 크롤링\n")
+print(" 2 : 구글 크롤링\n")
+select = input(" 선택 : ")
 
-# 나무위키 크롤링 2)
-# for no in range(1, len(sheet["A"]) + 1):
-#     if sheet["B"+str(no)].value is None:
-#         print("제목 : ", sheet["A"+str(no)].value)
-#         namuwiki_crawl(work_book, "ani_a.xlsx", sheet["H" + str(no)].value, sheet["B" + str(no)])
+# 나무위키 크롤링
+if select == "1":
+    xlsx_list = ["ani_c.xlsx", "ani_d.xlsx", "ani_e.xlsx", "ani_f.xlsx", "ani_g.xlsx", "ani_h.xlsx", "ani_i.xlsx",
+                 "ani_j.xlsx", "ani_k.xlsx", "ani_l.xlsx", "ani_m.xlsx", "ani_n.xlsx", "ani_o.xlsx", "ani_p.xlsx"]
 
-# namuwiki_crawl(work_book, "ani_a.xlsx", sheet["H86"].value, sheet["B86"])
+    for xl in xlsx_list:
+        work_book = load_workbook(xl)
+        sheet = work_book['Sheet']
+        for no in range(2, len(sheet["A"]) + 1):
+            namuwiki_crawl(work_book, xl, sheet["H"+str(no)].value, sheet["B"+str(no)])
 
 # 구글 크롤링
-
-# 건강 전라계 수영부 우미쇼 38
-# 게이트 - 자위대. 그의 땅에서, 이처럼 싸우며 55
-# 고식 73
-# 골프천재 탄도 86
-# 괴짜가족 107
-# 귀를 기울이면 109
-# 구구레 코쿠리상 = 나와라! 코쿠리상 114
-# 구인사가 119
-# 구명전사 제노사이버 117
-# 귀신전 123
-# 그래플러 바키 140
-# 금색의 갓슈 151
-# 기분 나쁜 모노노케안 168
-
-#글라스립 153
-#그러나 죄인은 용과 춤춘다
+if select == "2":
+    pass
